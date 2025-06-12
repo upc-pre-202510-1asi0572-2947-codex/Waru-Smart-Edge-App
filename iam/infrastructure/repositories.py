8614d2daa6b9031234fdd3dd38ccb525a5258439
fdaniel13s <u20221c628@upc.edu.pe>
2025-06-12 12:04:22 -0500

    refactor: refactor bc IAM

--- iam/infrastructure/repositories.py
+++ iam/infrastructure/repositories.py
@@ -1,31 +1,34 @@
-from datetime import datetime
-from typing import Optional
-
+"""Repository for managing device entities in the IAM context."""
 import peewee
-
+from typing import Optional
 from iam.domain.entities import Device
 from iam.infrastructure.models import Device as DeviceModel
 
-
 class DeviceRepository:
     @staticmethod
-    def find_by_id_and_api_key(device_id:str, api_key:str) -> Optional[Device]:
-        """find a device by itsID and api_key
+    def find_by_id_and_api_key(device_id: str, api_key: str) -> Optional[Device]:
+        """find a device by its ID and API key.
+
+        Args:
+            device_id (str): The unique identifier for the device.
+            api_key (str): The API key associated with the device.
+
+        Returns:
+            Device: The device if found, otherwise None.
         """
         try:
-            device =DeviceModel.get(
-                (DeviceModel.device_id == device_id) &
-                (DeviceModel.api_key == api_key)
-            )
+            device = DeviceModel.get((DeviceModel.device_id == device_id) & (DeviceModel.api_key == api_key))
             return Device(device.device_id, device.api_key, device.created_at)
         except peewee.DoesNotExist:
             return None
 
     @staticmethod
-    def get_or_create_test_device()->Device:
+    def get_or_create_test_device() -> Device:
+        """Get or create a test device for development purposes.
+        Returns:
+            Device: A test device with a predefined ID and API key.
+        """
         device, _ = DeviceModel.get_or_create(
-            device_id='smart-band-002340i9j',
-            defaults={"api_key": "test-api-key", "created_at": datetime.now()}
-        )
+            device_id='smart-band-001',
+            defaults={"api_key": "test-api-key-123", "created_at": "2025-06-06T12:00:00Z"})
         return Device(device.device_id, device.api_key, device.created_at)
-
--- iam/interfaces/services.py
+++ iam/interfaces/services.py
@@ -1,15 +1,27 @@
-from flask import request, jsonify, Blueprint
+"""Interfaces for IAM services."""
+from flask import Blueprint, request, jsonify, Response
 
 from iam.application.services import AuthApplicationService
 
-iam_api: Blueprint = Blueprint('iam_api', __name__)
+"""Blueprint for IAM service interfaces."""
+iam_api = Blueprint('iam_api', __name__)
+
+"""Initialize dependencies for IAM service."""
 auth_service = AuthApplicationService()
 
-def authenticate_request():
-    device_id = request.json.get("device_id") if request.json else None
-    api_key = request.headers.get("X-API-Key")
+def authenticate_request() -> None | tuple[Response, int]:
+    """Authenticate the incoming request using device ID and API key.
+    This function checks for the presence of 'device_id' in the request JSON
+    and 'X-API-Key' in the request headers.
+    If either is missing or invalid, it returns 401 Unauthorized responses.
+    Returns:
+        None: If authentication is successful, returns None.
+        Response: If authentication fails, returns a JSON response with an error message.
+    """
+    device_id   = request.json.get('device_id') if request.json else None
+    api_key     = request.headers.get('X-API-Key')
     if not device_id or not api_key:
-        return jsonify({"error": "No device id or API key provided"}), 401
+        return jsonify({"error": "Device ID and API key are required"}), 401
     if not auth_service.authenticate(device_id, api_key):
-        return jsonify({"error": "Invalid API key provided"}), 401
+        return jsonify({"error": "Invalid device_id or API key"}), 401
     return None
